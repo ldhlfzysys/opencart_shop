ffe7a62354a655c247ea2e6de972e39445d423fd
ldhlfzysys <ldhlfzysys@163.com>
2018-06-19 21:12:28 +0800

    三方支付

--- admin/language/zh-cn/extension/payment/custom_pay.php
+++ admin/language/zh-cn/extension/payment/custom_pay.php
@@ -1,23 +1,32 @@
 <?php
 // Heading
-$_['heading_title']		 = '第三方微信支付';
+$_['heading_title']                  = '第三方微信支付';
 
 // Text
-$_['text_extension']	 = '扩展功能';
-$_['text_success']		 = '成功: 已修改银行转账信息！';
-$_['text_edit']          = '编辑银行转账 - (在线教程: <a href="http://www.mycncart.com/blog-120.html" target="_blank">http://www.mycncart.com/blog-120.html</a>)';
+$_['text_extension']                 = 'Extensions';
+$_['text_success']                   = 'Success: You have modified Alipay account details!';
+$_['text_edit']                      = 'Edit Alipay Pay';
+$_['text_alipay_cross']              = '<a target="_BLANK" href="https://global.alipay.com"><img src="view/image/payment/alipay-cross-border.png" alt="Alipay Pay Website" title="Alipay Pay Website" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_live']                      = 'Live';
+$_['text_sandbox']                   = 'Sandbox';
 
 // Entry
-$_['entry_bank']		 = '银行转账指导信息';
-$_['entry_total']		 = '总额';
-$_['entry_order_status'] = '订单状态';
-$_['entry_geo_zone']	 = '区域群组';
-$_['entry_status']		 = '状态';
-$_['entry_sort_order']	 = '排序';
+$_['entry_app_id']                   = 'Partner ID';
+$_['entry_merchant_private_key']     = 'Key';
+$_['entry_test']                     = 'Test mode';
+$_['entry_total']                    = 'Total';
+$_['entry_currency']                 = 'Currency Code';
+$_['entry_order_status']             = 'Completed Status';
+$_['entry_geo_zone']                 = 'Geo Zone';
+$_['entry_status']                   = 'Status';
+$_['entry_sort_order']               = 'Sort Order';
 
 // Help
-$_['help_total']		 = '若使该支付方式有效，需要订单金额至少达到此金额。';
+$_['help_total']                     = 'The checkout total the order must reach before this payment method becomes active';
+$_['help_currency']                  = 'The settlement currency code the merchant specifies in the contract. You can add a new currency in System-&gt;Localisation-&gt;Currency if your settlement currency is not in the list';
+$_['help_alipay_setup']              = '<a target="_blank" href="http://www.opencart.cn/docs/alipay">Click here</a> to learn how to set up Alipay account.';
 
 // Error
-$_['error_permission']   = '警告: 无权限修改支付方式银行转账！';
-$_['error_bank']         = '银行转账指导信息必填！';
+$_['error_permission']               = 'Warning: You do not have permission to modify pay Alipay!';
+$_['error_app_id']                   = 'Partner ID required!';
+$_['error_merchant_private_key']     = 'Key required!';
